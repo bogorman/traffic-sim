fc0dce50173008f9c4f9c1ca7dc7da032a56cc68
Szymon Łabuz <szimin18@gmail.com>
2016-04-15 17:02:42 +0200

    Imported upicle correctly

--- client/src/main/scala/client/js/FrontClient.scala
+++ client/src/main/scala/client/js/FrontClient.scala
@@ -4,9 +4,11 @@ import org.scalajs.dom
 
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom.all._
+import autowire.Client
+import upickle.{Js, ReaderPicker, WriterPicker}
 
 @JSExport
-object FrontClient {
+object FrontClient extends Client {
 
   def myContent = div(
     h1(id := "title", "This is a title"),
@@ -18,5 +20,19 @@ object FrontClient {
 
     dom.document.getElementById("scalaMagicClientCode").textContent = "Victory!"
     dom.document.getElementById("scalaMagicClientCode").appendChild(myContent)
+  }
+
+  def write[Result: WriterPicker](r: Result) = upickle.default.write(r)
+  def read[Result: ReaderPicker](p: String) = upickle.default.read[Result](p)
+
+  override def doCall(req: Request) = {
+    println(req)
+    
+    dom.ext.Ajax.post(
+      url = "/api/" + req.path.mkString("/"),
+      data = upickle.json.write(Js.Obj(req.args.toSeq:_*))
+    ).map(_.responseText)
+      .map(upickle.json.read)
+
   }
 }
--- server/app/system/ApiImplementation.scala
+++ server/app/system/ApiImplementation.scala
@@ -1,16 +1,17 @@
 package system
 
+import autowire.Server
 import shared.map.MapApi
-import upickle._
-import autowire._
+import upickle.{ReaderPicker, WriterPicker}
 
-object ApiImplementation extends MapApi{
-  override def test: String = "Takie zajebiste Ajaxy"
+object ApiImplementation extends MapApi {
+  override def test: String = "Takie zajebiste Ajaxy ktore nie dzialaja"
 }
 
-object MyServer extends autowire.Server[String, upickle.Reader, upickle.Writer]{
-  def write[Result: Writer](r: Result) = upickle.write(r)
-  def read[Result: Reader](p: String) = upickle.read[Result](p)
+object MyServer extends Server[String, ReaderPicker, WriterPicker] {
+  def write[Result: WriterPicker](r: Result) = upickle.default.write(r)
 
-  val routes = MyServer.route[MyApi](MyApiImpl)
+  def read[Result: ReaderPicker](p: String) = upickle.default.read[Result](p)
+
+  val routes = MyServer.route[MapApi](ApiImplementation)
 }
