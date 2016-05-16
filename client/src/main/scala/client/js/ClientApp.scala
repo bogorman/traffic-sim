e2ed7880cce5b891cb3ed0b2793368d10abe3ff9
tlegutko <tom.legutko@gmail.com>
2016-05-16 18:39:32 +0200

    Root path again available.

--- client/src/main/scala/client/js/ClientApp.scala
+++ client/src/main/scala/client/js/ClientApp.scala
@@ -14,7 +14,7 @@ import scala.scalajs.js
 import scala.util.{Failure, Success}
 import scalatags.JsDom.all._
 
-object Client extends Client[Js.Value, Reader, Writer] {
+object ClientApi extends Client[Js.Value, Reader, Writer] {
   def read[Result: Reader](p: Js.Value) = upickle.default.readJs[Result](p)
 
   def write[Result: Writer](r: Result) = upickle.default.writeJs(r)
@@ -29,18 +29,17 @@ object Client extends Client[Js.Value, Reader, Writer] {
 }
 
 
-object FrontClient extends js.JSApp {
+object ClientApp extends js.JSApp {
   def main(): Unit = {
-    val textContent = Client[MapApi].test().call().onComplete {
-      //      case Success(succ) => dom.document.getElementById("scalaMagicClientCode").textContent = succ
+    val textContent = ClientApi[MapApi].test().call().onComplete {
       case Success(succ) => println(s"success: $succ")
       case Failure(fail) => println(s"failure: $fail")
     }
-    //    dom.document.body.appendChild(myContent)
+
     val mapCanvas = canvas(id := "mapCanvas", width := 900, height := 900).render
     dom.document.body.appendChild(mapCanvas)
-    val context = mapCanvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
-    MapViewer.drawMap(context, new RoadMap(List.empty, List.empty))
+    val context2D = mapCanvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+    MapViewer.drawMap(context2D, new RoadMap(List.empty, List.empty))
   }
 
   def createWebSocket(address: String): dom.WebSocket = {
