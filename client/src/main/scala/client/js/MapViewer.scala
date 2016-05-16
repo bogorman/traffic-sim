df9e72792add6e126a0dc2c142e20007e693d1b5
Szymon Łabuz <szimin18@gmail.com>
2016-05-16 21:05:09 +0200

    Modeled basic cars

--- client/src/main/scala/client/js/MapViewer.scala
+++ client/src/main/scala/client/js/MapViewer.scala
@@ -1,8 +1,11 @@
 package client.js
 
 import org.scalajs.dom.raw.CanvasRenderingContext2D
+import shared.car.CarsList
 import shared.geometry._
 import shared.map.RoadMap
+
+import scala.util.Random
 
 object MapViewer {
   private val MapCoordinatesRange = 1000.0
@@ -12,6 +15,9 @@ object MapViewer {
   private val PixelsPerMapStep = PixelsMapRange / MapCoordinatesRange
 
   private val HalfCrossingSize = 20.0
+  private val HalfCarSize = 5.0
+
+  private val colorsList = List("green", "red", "blue", "yellow", "pink")
 
   def drawMap(context: CanvasRenderingContext2D, map: RoadMap): Unit = {
     context.font = HalfCrossingSize + "px Arial"
@@ -24,8 +30,6 @@ object MapViewer {
       val allPoints = road.start.coordinates :: road.bendingPoints ::: road.end.coordinates :: List.empty
       allPoints.sliding(2).foreach { case List(start, end) => drawRoad(context, start, end) }
     })
-
-    context.stroke
   }
 
   def drawCrossing(context: CanvasRenderingContext2D, x: Double, y: Double, name: String): Unit = {
@@ -50,8 +54,29 @@ object MapViewer {
     val scaledEndX = PixelsForMargins + end.x * PixelsPerMapStep
     val scaledEndY = PixelsForMargins + end.y * PixelsPerMapStep
 
+    context.beginPath
     context.moveTo(scaledStartX, scaledStartY)
     context.lineTo(scaledEndX, scaledEndY)
+    context.closePath
+    context.stroke
+
+    drawCar(context, (start.x + end.x) / 2.0 >< (start.y + end.y) / 2.0, colorsList(Random.nextInt(colorsList.size)))
+  }
+
+  def drawCars(context: CanvasRenderingContext2D, carsList: CarsList): Unit = {
+    carsList.cars.foreach(car => drawCar(context, car.location, car.color))
+  }
+
+  def drawCar(context: CanvasRenderingContext2D, location: Coordinates, color: String): Unit = {
+    val scaledX = PixelsForMargins + location.x * PixelsPerMapStep
+    val scaledY = PixelsForMargins + location.y * PixelsPerMapStep
+
+    context.fillStyle = color
+
+    context.fillRect(scaledX - HalfCarSize, scaledY - HalfCarSize, 2 * HalfCarSize, 2 * HalfCarSize)
+    context.strokeRect(scaledX - HalfCarSize, scaledY - HalfCarSize, 2 * HalfCarSize, 2 * HalfCarSize)
+
+    context.fillStyle = "#000000"
   }
 }
 
