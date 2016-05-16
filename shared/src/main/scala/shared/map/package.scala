78144b876c78d75056f7f4be70282ebd6be69c45
Pawel Marks <pmarks@student.agh.edu.pl>
2016-05-16 20:07:16 +0200

    fixed NPE

--- shared/src/main/scala/shared/map/package.scala
+++ shared/src/main/scala/shared/map/package.scala
@@ -1,8 +1,8 @@
 package shared
 
-import scala.math.pow
-import scala.language.postfixOps
 import shared.geometry._
+
+import scala.language.postfixOps
 
 package object map {
 
@@ -12,27 +12,32 @@ package object map {
 
   class RoadMap(crossingDefs: List[CrossingDef], roadDefs: List[RoadDef]) {
 
-    private[map] val roadMap: Map[String, List[Road]] = roadDefs groupBy { _.start } mapValues { _ map { new Road(this, _) } }
-
-    private[map] val reverseRoadMap: Map[String, List[Road]] = roadMap.values.flatten.toList groupBy { _.end.name }
-
     val crossingsMap: Map[String, Crossing] = crossingDefs map { c => c.name -> new Crossing(this, c) } toMap
+    private[map] val roadMap: Map[String, List[Road]] = roadDefs groupBy {
+      _.start
+    } mapValues {
+      _ map {
+        new Road(this, _)
+      }
+    }
+    private[map] val reverseRoadMap: Map[String, List[Road]] = roadMap.values.flatten.toList groupBy {
+      _.end.name
+    }
 
     def crossings: List[Crossing] = crossingsMap.values.toList
 
     def roads: List[Road] = roadMap.values.flatten.toList.distinct
+
+    override def toString: String = s"{$roadMap}"
   }
 
   class Road(map: RoadMap, definition: RoadDef) {
-    print(map)
 
-    print(definition)
+    def length: Double = (start.coordinates :: bendingPoints ::: end.coordinates :: Nil) sliding 2 map { cs => distance(cs.head, cs.last) } sum
 
     def start: Crossing = map.crossingsMap(definition.start)
 
     def end: Crossing = map.crossingsMap(definition.end)
-
-    def length: Double = (start.coordinates :: bendingPoints ::: end.coordinates :: Nil) sliding 2 map { cs => distance(cs.head, cs.last)} sum
 
     def bendingPoints: List[Coordinates] = definition.bendingPoints
 
@@ -41,9 +46,9 @@ package object map {
 
   class Crossing(private val map: RoadMap, private val definition: CrossingDef) {
 
-    def roads: List[Road] = map.roadMap(definition.name)
+    def roads: List[Road] = map.roadMap.getOrElse(definition.name, List.empty)
 
-    def reverseRoads: List[Road] = map.reverseRoadMap(definition.name)
+    def reverseRoads: List[Road] = map.reverseRoadMap.getOrElse(definition.name, List.empty)
 
     def name: String = definition.name
 
@@ -51,7 +56,7 @@ package object map {
 
     override def equals(obj: scala.Any): Boolean = obj match {
       case that: Crossing => (this.map eq that.map) && this.definition == that.definition
-      case _              => false
+      case _ => false
     }
 
     override def hashCode(): Int = map.## * 37 + definition.## * 7
